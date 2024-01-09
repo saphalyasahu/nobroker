bf5650c69d2b461e55f4d9e9617aa48fcdeb4dff
saphalya <saphalyasahu@gmail.com>
2024-01-09 12:03:50 +0530

    9th jan

---
+++ nobroker/src/main/java/com/nobroker/service/OwnerPlanService.java
@@ -0,0 +1,8 @@
+package com.nobroker.service;
+
+import com.nobroker.entity.OwnerPlan;
+import com.nobroker.payload.OwnerPlanDto;
+
+public interface OwnerPlanService {
+    public OwnerPlan createOwnerPlans(OwnerPlanDto ownerPlanDto);
+}
